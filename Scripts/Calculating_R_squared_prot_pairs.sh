9393380babc4a5506d5950697155e04ba8f7f088
Hannah Wilson <hannah.wilson@bristol.ac.uk>
2021-05-12 12:54:36 +0100

    Changed r version for .sh file

--- Scripts/Calculating_R_squared_prot_pairs.sh
+++ Scripts/Calculating_R_squared_prot_pairs.sh
@@ -10,7 +10,7 @@
 
 
 echo "Running on ${HOSTNAME}"
-module add languages/r/4.0.3 
+module add languages/r/3.6.2
 Rscript Calculating_R_squared_prot_pairs.R /mnt/storage/scratch/hw15842/repo/L1000_protein_pair_trait_MR/Data /mnt/storage/scratch/hw15842/protein_interactions/network_comparisons/Data
 
 
