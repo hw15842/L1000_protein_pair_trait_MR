7b6d45e5c96ee4e914cbe4277942095caad85cd9
Hannah Wilson <hannah.wilson@bristol.ac.uk>
2021-05-12 12:05:10 +0100

    Adding scripts used to calculate the R squared results

---
+++ Scripts/Calculating_R_squared_prot_pairs.R
@@ -0,0 +1,61 @@
+############################################
+#### Calculating_R_squared_prot_pairs.R ####
+############################################
+
+
+args  <-  commandArgs(trailingOnly=TRUE)
+results_location <- toString(args[1])
+data_location <- toString(args[2])
+
+
+library(dplyr)
+library(TwoSampleMR)
+
+setwd(paste0(results_location) ### saveing these to the "data" folder in the git hub repository to use with the rmarkdown script 
+
+load(paste0(data_location, "Zheng_pQTLs_with_file_names.rdata")) ## not adding this file or the significant MRs to the Data folder as sig MRs too big so just extracting them from their original locations 
+load(paste0(data_location, "significant_MRs.rdata"))
+
+
+exposure_ids <- as.character(unique(Zheng_pQTLs_with_file_names$prot_file_name))
+outcome_ids <- as.character(unique(significant_MRs$lower.outcome))
+
+expdat <- lapply(exposure_ids, extract_instruments) %>% bind_rows()
+
+save(expdat, file="expdat.rdata")
+
+l <- list()
+m <- list()
+for(i in 1:length(outcome_ids))
+{
+  # get list of exposures to test on outcome
+  exps <- subset(significant_MRs, id.outcome == outcome_ids[i])$id.exposure %>% unique()
+  # Get instruments for the exposures that you need for this outcome
+  snps <- subset(expdat, id.exposure  %in% exps)$SNP
+  
+  # extract
+  l[[i]] <- extract_outcome_data(snps, outcome_ids[i])
+  
+  # harmonise
+  e <- subset(expdat, exps %in% id.exposure)
+  m[[i]] <- harmonise_data(e, l[[i]], action=1)
+}
+
+outdat <- bind_rows(l)
+
+save(outdat, file="outdat.rdata")
+
+exp_out_dat<- bind_rows(m)
+
+save(exp_out_dat, file="exp_out_dat.rdata"
+
+# get rsq
+
+rsq_dat <- steiger_filtering(exp_out_dat)
+
+save(rsq_dat, file="rsq_dat.rdata")
+
+
+
+# save dat expdat and outdat so you don't need to extract again
+
---
+++ Scripts/Calculating_R_squared_prot_pairs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=rsq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=12:00:00
+#SBATCH --mem=100000M
+#SBATCH --partition=cpu
+
+
+echo "Running on ${HOSTNAME}"
+module add languages/r/4.0.3 
+Rscript Calculating_R_squared_prot_pairs.R /mnt/storage/scratch/hw15842/repo/L1000_protein_pair_trait_MR/Data /mnt/storage/scratch/hw15842/protein_interactions/network_comparisons/Data
+
+
+
