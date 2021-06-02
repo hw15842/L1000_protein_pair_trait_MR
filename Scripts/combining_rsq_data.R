ed6873071957210ff4a1ec2b878cb8216d930838
Hannah Wilson <hannah.wilson@bristol.ac.uk>
2021-06-02 16:11:37 +0100

    combining the rsq data scripts

---
+++ Scripts/combining_rsq_data.R
@@ -0,0 +1,25 @@
+##############################
+#### combining_rsq_data.R ####
+##############################
+
+
+args  <-  commandArgs(trailingOnly=TRUE)
+data_location <- toString(args[1])
+
+setwd(paste0(data_location))
+
+library(dplyr)
+library(plyr)
+
+
+
+file_list_rsq_dat <- list.files(path=paste0(data_location), pattern=glob2rx("rsq_dat_table_*.rdata"))
+
+rsq_dat <- lapply(file_list_rsq_dat,function(x){
+     df <- get(load(x))})
+
+
+rsq_dat <- bind_rows(rsq_dat)
+
+
+save(rsq_dat, file=paste0(data_location, "rsq_dat.rdata"))
