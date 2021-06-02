3cfdc422d0b87ee0701c0e8a0a46375567549afd
Hannah Wilson <hannah.wilson@bristol.ac.uk>
2021-06-02 16:18:12 +0100

    re uploading script

--- Scripts/combining_rsq_data.R
+++ Scripts/combining_rsq_data.R
@@ -15,11 +15,16 @@ library(plyr)
 
 file_list_rsq_dat <- list.files(path=paste0(data_location), pattern=glob2rx("rsq_dat_table_*.rdata"))
 
+head(file_list_rsq_dat)
+
 rsq_dat <- lapply(file_list_rsq_dat,function(x){
      df <- get(load(x))})
 
 
 rsq_dat <- bind_rows(rsq_dat)
+
+head(rsq_dat)
+nrow(rsq_dat)
 
 
 save(rsq_dat, file=paste0(data_location, "rsq_dat.rdata"))
