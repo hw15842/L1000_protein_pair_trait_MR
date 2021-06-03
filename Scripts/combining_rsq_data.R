12842b17426d63b8329d602dee24138def576f20
Hannah Wilson <hannah.wilson@bristol.ac.uk>
2021-06-03 12:05:01 +0100

    troub;le shooting why not running

--- Scripts/combining_rsq_data.R
+++ Scripts/combining_rsq_data.R
@@ -5,6 +5,8 @@
 args  <-  commandArgs(trailingOnly=TRUE)
 data_location <- toString(args[1])
 
+# data_location <- "/mnt/storage/scratch/hw15842/repo/L1000_protein_pair_trait_MR/Data"
+
 setwd(paste0(data_location))
 
 library(dplyr)
@@ -13,13 +15,16 @@ library(plyr)
 
 
 file_list_rsq_dat <- list.files(path=paste0(data_location), pattern=glob2rx("rsq_dat_table_*.rdata"))
+head(file_list_rsq_dat)
 
 rsq_dat <- lapply(file_list_rsq_dat,function(x){
      df <- get(load(x))})
 
+dim(rsq_dat)
 
 rsq_dat <- bind_rows(rsq_dat)
-
+head(rsq_dat)
+nrow(rsq_dat)
 
 save(rsq_dat, file=paste0(data_location, "rsq_dat.rdata"))
 
